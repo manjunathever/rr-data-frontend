1a4fe8b63ae2d0ec612db66056b371791b45c1ad
manjunathBase2 <manjunathworkspace@gmail.com>
2024-07-04 04:00:10 +0530

    trying without clinical

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,13 @@
-import {resolve} from 'path' 
+// import {resolve} from 'path' 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const root = resolve(__dirname, 'src')
-const outDir = resolve(__dirname, 'dist')
+// const root = resolve(__dirname, 'src')
+// const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
   plugins: [react()],
-  // base: '/dist',
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(root, 'index.html'),
-        clinical: resolve(root, 'clinical', 'index.html'),
-      }
-    }
-  }
+
 })
 
